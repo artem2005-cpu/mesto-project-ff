1e17bd6756abf0e551d487b6be2f2c196d1fbd41
Artem Voinov <artemvojnov8@gmail.com>
2024-06-26 16:04:10 +0400

    Add: createCard and addCard functions

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const createCard = () => {
+	const cards = initialCards.concat()
+	cards.forEach(element => {
+		addCard(element.link, element.name)
+	})
+}
+
+const addCard = (link, name) => {
+	const placesList = document.querySelector('.places__list')
+	const cardTemplate = document
+		.querySelector('#card-template')
+		.content.cloneNode(true)
+	const cardImage = cardTemplate.querySelector('.card__image')
+	const cardName = cardTemplate.querySelector('.card__title')
+	const removeButton = cardTemplate.querySelector('.card__delete-button')
+	cardImage.src = link
+	cardName.textContent = name
+	removeButton.addEventListener('click', () =>
+		removeButton.closest('.card').remove()
+	)
+	placesList.append(cardTemplate)
+}
+createCard()
